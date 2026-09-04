ad8fba55cdf9c288a5c7d9dc753f2982ce642399
Purva <purva60905@gmail.com>
2026-09-04 09:27:50 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,7 @@ const app =
    MIDDLEWARE
 ========================================= */
 
-app.use(
-    cors()
-);
+app.use(cors());
 
 
 app.use(
@@ -177,20 +175,11 @@ mongoose
             await createDefaultAdmin();
 
 
-            const PORT =
-                process.env.PORT || 5000;
+            const PORT = process.env.PORT || 5000;
 
-
-            app.listen(
-                PORT,
-                () => {
-
-                    console.log(
-                        `Server running on port ${PORT}`
-                    );
-
-                }
-            );
+                app.listen(PORT, () => {
+                    console.log(`Server running on port ${PORT}`);
+            });
 
         }
     )
